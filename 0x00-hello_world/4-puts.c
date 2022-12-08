a727f9f17b03e22f70e5bbf8426ddb0f85a23d90
jeancherono96 <jeancherono96@gmail.com>
2022-12-08 13:20:17 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main-main block
- * return:0
+ * main - Entry
+ * return: Always 0
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,1 +1,1 @@
-Write a script that compiles a C file but does not link
+Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
