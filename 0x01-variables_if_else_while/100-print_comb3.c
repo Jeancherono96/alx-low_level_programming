593b4d7e910dac3400b0f7ddbabb7878d4ca5b1d
jeancherono96 <jeancherono96@gmail.com>
2022-12-11 11:38:40 -0800

    last

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,8 +14,12 @@ int main(void)
 		{
 			putchar(tens + '0');
 			putchar(ones + '0');
+			if (tens < 8)
+			{
+				putchar(',');
+					putchar(' ');
 		}
 	}
-	putchar('\n');	
-	return (0);
-}
+		putchar('\n');	
+		return (0);	
+	}
